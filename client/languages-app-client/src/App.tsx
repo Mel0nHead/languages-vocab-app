4d25bfba170634d25223f1ccee12bfd673484298
Alex King <alexking@Alexs-MacBook-Pro.local>
2020-03-27 14:31:51 +0000

    add routes

--- client/languages-app-client/src/App.tsx
+++ client/languages-app-client/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Review } from "./pages/Review";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/home" exact component={Home} />
+        <Route path="/review" exact component={Review} />
+        <Route path="*" render={() => <h1>Page not found</h1>} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-
-export default App;
