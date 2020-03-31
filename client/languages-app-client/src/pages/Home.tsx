d0a2516e32bc643224d626d7dc3d782c30ac558f
Alex King <alexking@Alexs-MacBook-Pro.local>
2020-03-31 11:46:46 +0100

    add destination and source languages

--- client/languages-app-client/src/pages/Home.tsx
+++ client/languages-app-client/src/pages/Home.tsx
@@ -15,7 +15,10 @@ export function Home() {
   const [availableLanguages, setAvailableLanguages] = useState({
     en: "English"
   });
-  const [currentLanguage, setCurrentLanguage] = useState("en");
+  const [currentLanguage, setCurrentLanguage] = useState({
+    source: "en",
+    destination: "en"
+  });
   const [words, setWords] = useState<Word[]>([]);
 
   function getSupportedLanguages() {
@@ -47,15 +50,32 @@ export function Home() {
     setInputValue(event.target.value);
   }
 
-  function handleLanguageChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    setCurrentLanguage(event.target.value);
+  function handleSourceLanguageChange(
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) {
+    const source = event.target.value;
+    setCurrentLanguage(currentLanguage => ({
+      ...currentLanguage,
+      source
+    }));
+  }
+
+  function handleDestinationLanguageChange(
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) {
+    const destination = event.target.value;
+    setCurrentLanguage(currentLanguage => ({
+      ...currentLanguage,
+      destination
+    }));
   }
 
   function handleTranslate() {
     if (!inputValue) return;
     const textToTranslate = encodeURI(inputValue);
+    const languageString = `${currentLanguage.source}-${currentLanguage.destination}`;
     fetch(
-      `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${yandexApiKey}&lang=${currentLanguage}&text=${textToTranslate}`,
+      `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${yandexApiKey}&lang=${languageString}&text=${textToTranslate}`,
       { method: "POST" }
     )
       .then(response => {
@@ -68,8 +88,9 @@ export function Home() {
       .then(data => {
         console.log(data);
         const id = Math.random();
+        const languageString = `${currentLanguage.source}-${currentLanguage.destination}`;
         const word = {
-          language: currentLanguage,
+          language: languageString,
           originalWord: inputValue,
           translatedWord: data.text[0],
           id
@@ -89,8 +110,24 @@ export function Home() {
       <h1>Home</h1>
       <input type="text" value={inputValue} onChange={handleInputChange} />
       <div>
-        <span>Select language to translate to:</span>
-        <select value={currentLanguage} onChange={handleLanguageChange}>
+        <span>From:</span>
+        <select
+          value={currentLanguage.source}
+          onChange={handleSourceLanguageChange}
+        >
+          {Object.entries(availableLanguages).map(language => {
+            return (
+              <option key={language[0]} value={language[0]}>
+                {language[1]}
+              </option>
+            );
+          })}
+        </select>
+        <span>To:</span>
+        <select
+          value={currentLanguage.destination}
+          onChange={handleDestinationLanguageChange}
+        >
           {Object.entries(availableLanguages).map(language => {
             return (
               <option key={language[0]} value={language[0]}>
@@ -105,7 +142,10 @@ export function Home() {
       </div>
       <div>
         {words.map(word => (
-          <div key={word.id}>
+          <div
+            key={word.id}
+            style={{ border: "1px solid red", marginBottom: "20px" }}
+          >
             <span>{`${word.originalWord} - ${word.translatedWord}`}</span>
             <br />
             <span>{word.language}</span>
