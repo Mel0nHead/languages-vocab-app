5c16c287c03fdb54ce3b46a239faa03263507e46
Alex King <alexking@Alexs-MacBook-Pro.local>
2020-04-30 18:08:52 +0100

    get it working

---
+++ client/languages-app-client/src/graphql/useGetNextWordQuery.ts
@@ -0,0 +1,30 @@
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
+
+export function useGetNextWordQuery(first: number, after: string | null) {
+  return useQuery(
+    gql`
+      query getWords($first: Int, $after: String) {
+        getAllWords(first: $first, after: $after) {
+          edges {
+            node {
+              id
+              originalWord
+              translatedWord
+              dateLastSeen
+              dateAdded
+              box
+              language
+            }
+            cursor
+          }
+          pageInfo {
+            hasPreviousPage
+            hasNextPage
+          }
+        }
+      }
+    `,
+    { variables: { first, after } }
+  );
+}
--- client/languages-app-client/src/pages/Test.tsx
+++ client/languages-app-client/src/pages/Test.tsx
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 import { Typography, Button, Paper } from "@material-ui/core";
+import { useGetNextWordQuery } from "../graphql/useGetNextWordQuery";
+
+/**
+ * TODO:
+ * - add styling
+ * - add some tests
+ * - refactor this mess
+ */
 
 export function Test() {
   const [testInProgress, setTestInProgress] = useState(false);
+  const [isTestFinished, setIsTestFinished] = useState(false);
+  const [cursor, setCursor] = useState<string | null>(null);
+  const [isRevealed, setIsRevealed] = useState(false);
+  const { data, error, loading } = useGetNextWordQuery(1, cursor);
 
   function getNotInProgressMessage() {
     return (
@@ -24,27 +36,81 @@ export function Test() {
     );
   }
 
+  function getTestIsFinishedMessage() {
+    return (
+      <div>
+        <Typography>
+          You have finished the test! Click the button below to start a new
+          test.
+        </Typography>
+        <div>
+          <Button
+            variant="contained"
+            onClick={() => {
+              setTestInProgress(true);
+              setIsTestFinished(false);
+              setCursor(null);
+            }}
+          >
+            Start Test
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
   function getTestContent() {
+    if (error) {
+      return <b>An error occurred: {error.message}</b>;
+    }
+
+    if (loading) {
+      return <b>Loading...</b>;
+    }
+
+    const currentWord = data?.getAllWords?.edges[0].node;
+    const currentWordCursor = data?.getAllWords?.edges[0].cursor;
+    const hasNextPage = data?.getAllWords.pageInfo.hasNextPage;
+
     return (
       <Paper>
         <div>
           <Typography>What is the translation of the word below?</Typography>
         </div>
         <div>
-          <div>Word info</div>
           <div>
-            <Button>Reveal answer</Button>
+            <div>{currentWord.originalWord}</div>
+            <div>{currentWord.language}</div>
+            {isRevealed && <div>{currentWord.translatedWord}</div>}
+          </div>
+          <div>
+            <Button onClick={() => setIsRevealed(!isRevealed)}>
+              Reveal answer
+            </Button>
           </div>
         </div>
-        <div>
+        {isRevealed && (
           <div>
             <Typography>Did you get the answer right?</Typography>
+            <div>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  setIsRevealed(false);
+                  if (hasNextPage) {
+                    setCursor(currentWordCursor);
+                  } else {
+                    setIsTestFinished(true);
+                    setTestInProgress(false);
+                  }
+                }}
+              >
+                Yes
+              </Button>
+              <Button variant="contained">No</Button>
+            </div>
           </div>
-          <div>
-            <Button variant="contained">Yes</Button>
-            <Button variant="contained">No</Button>
-          </div>
-        </div>
+        )}
       </Paper>
     );
   }
@@ -53,7 +119,8 @@ export function Test() {
     <div>
       <h1>Test</h1>
       <div>
-        {!testInProgress && getNotInProgressMessage()}
+        {!testInProgress && !isTestFinished && getNotInProgressMessage()}
+        {!testInProgress && isTestFinished && getTestIsFinishedMessage()}
         {testInProgress && getTestContent()}
       </div>
     </div>
