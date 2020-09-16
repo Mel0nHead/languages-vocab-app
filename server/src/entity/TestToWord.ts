3b2a5b30b82683dfebdb888de537b6c9658b8069
Alex King <alexking@Alexs-MacBook-Pro.local>
2020-09-16 11:00:18 +0100

    more stuff

--- server/src/entity/TestToWord.ts
+++ server/src/entity/TestToWord.ts
@@ -4,28 +4,30 @@ import {
   PrimaryGeneratedColumn,
   BaseEntity,
   ManyToOne,
+  PrimaryColumn,
+  JoinColumn,
 } from "typeorm";
 import { Word } from "./Word";
 import { Test } from "./Test";
 
 // This is used to specify a many-to-many relationship with custom properties
+// TODO: finish off the relationship and add resolvers. Currently following this: https://www.youtube.com/watch?v=8kZ7W-bI5qQ
 @Entity()
 export class TestToWord extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  testId: number;
-
-  @Column()
+  @PrimaryColumn()
   wordId: number;
+
+  @PrimaryColumn()
+  testId: number;
 
   @Column()
   isWordCorrect: boolean;
 
-  @ManyToOne((type) => Word, (word) => word.testToWords)
+  @ManyToOne((type) => Word, (word) => word.testToWords, { primary: true })
+  @JoinColumn({ name: "wordId" })
   word: Word;
 
-  @ManyToOne((type) => Test, (test) => test.testToWords)
+  @ManyToOne((type) => Test, (test) => test.testToWords, { primary: true })
+  @JoinColumn({ name: "testId" })
   test: Test;
 }
