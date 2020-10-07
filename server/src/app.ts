856fede7baf061502ba0b4dd153e33da3bb74fe6
Alex King <alexking@Alexs-MacBook-Pro.local>
2020-10-07 17:43:15 +0100

    start adding express-jwt middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express from "express";
+import expressJwt from "express-jwt";
 import bodyParser from "body-parser";
 import cors from "cors";
 import logger from "morgan";
@@ -8,26 +9,47 @@ import { ApolloServer } from "apollo-server-express";
 import { WordResolver } from "./resolvers/WordResolver";
 import { buildSchema } from "type-graphql";
 
+const PORT = 4000;
+
+interface UserRequest extends express.Request {
+  user: string;
+}
+
 const startServer = async () => {
   const schema = await buildSchema({
     resolvers: [WordResolver],
   });
 
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer({
+    schema,
+    context: ({ req }: { req: UserRequest }) => {
+      const user = req.user || null;
+      return { user };
+    },
+  });
+
   await createConnection();
 
   // create and setup express app
-  const port = 4000;
   const app = express();
   app.use(bodyParser.json());
   app.use(cors());
   app.use(logger("dev"));
 
+  // for authorisation and authentication
+  app.use(
+    expressJwt({
+      secret: "MY_SECRET",
+      algorithms: ["HS256"],
+      credentialsRequired: false,
+    })
+  );
+
   server.applyMiddleware({ app });
 
-  app.listen({ port }, () => {
+  app.listen({ port: PORT }, () => {
     console.log(
-      `Server ready at http://localhost:${port}${server.graphqlPath}`
+      `Server ready at http://localhost:${PORT}${server.graphqlPath}`
     );
   });
 };
