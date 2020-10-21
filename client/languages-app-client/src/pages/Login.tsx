6f75971084ee8379214081bbf26a75bbf00c9bd1
Alex King <alexking@Alexs-MacBook-Pro.local>
2020-10-21 16:54:44 +0100

    start adding formik to login page

--- client/languages-app-client/src/pages/Login.tsx
+++ client/languages-app-client/src/pages/Login.tsx
@@ -1,17 +1,18 @@
-import { Button, TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import { Field, Form, Formik } from "formik";
+import { TextField } from "formik-material-ui";
 import React from "react";
 
 export function Login() {
-  // TODO: add formik for validation
-  function handleSubmit() {
-    console.log("submitted");
-  }
 
   return (
-    <form>
-      <TextField label="Email" name="email" />
-      <TextField label="Password" name="password" />
-      <Button>Log in</Button>
-    </form>
+    <Formik initialValues={{ email: '', password: '' }} onSubmit={() => {
+      console.log('submitted')
+    }}>
+      <Form>
+        <Field component={TextField} name='email' label='Email' />
+       <Field component={TextField} name='password' label='Password' />
+      <Button type='submit'>Log in</Button></Form>
+    </Formik>
   );
 }
