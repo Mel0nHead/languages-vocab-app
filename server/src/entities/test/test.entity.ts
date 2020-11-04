d46baedb134677f745eedc6d8a4345f37a86b830
Alex King <alexking@Alexs-MacBook-Pro.local>
2020-11-04 08:58:27 +0000

    add test entity

---
+++ server/src/entities/test/test.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  ManyToOne,
+} from "typeorm";
+import { Field, ID, ObjectType } from "type-graphql";
+import { User } from "../user/user.entity";
+
+@ObjectType()
+@Entity()
+export class Test extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  //   @Field(() => User)
+  //   @ManyToOne(() => User, (user) => user.tests)
+  //   user: User;
+
+  @Field()
+  @Column()
+  createdAt: Date;
+
+  @Field()
+  @Column()
+  updatedAt: Date;
+
+  @Field({ nullable: true })
+  @Column()
+  finishedAt: Date;
+}
